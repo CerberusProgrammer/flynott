f8499a1d8182f3cf59c13d01666ea78489f3b293
sazarcode <cerberusprogrammer@gmail.com>
2023-07-04 19:17:19 -0700

    date update notes.

--- lib/components/providers/notes_provider.dart
+++ lib/components/providers/notes_provider.dart
@@ -6,19 +6,41 @@ import '../../infrastructure/models/note.dart';
 
 class NotesProvider extends ChangeNotifier {
   List<CategoryNote> categories = [];
+  bool toggleThemeDisplayNote = false;
+  bool isChanged = false;
   int indexCategory = 0;
   int indexNote = 0;
 
   void loadTestData() {
     categories.add(
       CategoryNote(
-        date: DateFormat('dd MMMM yyyy').format(DateTime.now()),
+        date: '03 Jul 2023',
         notes: [
-          Note(title: 'title', date: 'date', content: 'content', index: 0),
-          Note(title: 'title', date: 'date', content: 'content', index: 1),
-          Note(title: 'title', date: 'date', content: 'content', index: 2),
-          Note(title: 'title', date: 'date', content: 'content', index: 3),
-          Note(title: 'title', date: 'date', content: 'content', index: 4),
+          Note(
+              title: 'title',
+              date: '03 Jul 2023',
+              content: 'content',
+              index: 0),
+          Note(
+              title: 'title',
+              date: '10 May 2023',
+              content: 'content',
+              index: 1),
+          Note(
+              title: 'title',
+              date: '21 Apr 2023',
+              content: 'content',
+              index: 2),
+          Note(
+              title: 'title',
+              date: '13 Mar 2023',
+              content: 'content',
+              index: 3),
+          Note(
+              title: 'title',
+              date: '02 Feb 2023',
+              content: 'content',
+              index: 4),
         ],
         quantityNotes: 5,
         title: 'Material Design',
@@ -37,34 +59,48 @@ class NotesProvider extends ChangeNotifier {
     );
   }
 
+  Color getColorCategory() {
+    return categories[indexCategory].color;
+  }
+
+  void toggleThemeNote() {
+    toggleThemeDisplayNote = !toggleThemeDisplayNote;
+    notifyListeners();
+  }
+
   // Note functions.
   void updateNoteTitle(int indexCategory, int indexNote, String value) {
+    isChanged = true;
     categories[indexCategory].notes[indexNote].title = value;
   }
 
+  void updateDate() {
+    categories[indexCategory].notes[indexNote].date =
+        DateFormat('dd MMMM yyyy').format(DateTime.now());
+    isChanged = false;
+  }
+
   void updateNoteContet(int indexCategory, int indexNote, String value) {
+    isChanged = true;
     categories[indexCategory].notes[indexNote].content = value;
   }
 
   void removeNote(int indexCategory, int indexNote) {
     categories[indexCategory].notes.removeAt(indexNote);
+    categories[indexCategory].quantityNotes--;
+    notifyListeners();
   }
 
   void addNewNote(
-    int indexCategory,
-    String title,
-    String date,
-    String content,
-  ) {
+      int indexCategory, String title, String date, String content) {
     categories[indexCategory].quantityNotes++;
-    categories[indexCategory].notes.add(
-          Note(
+    categories[indexCategory].notes.insert(
+        0,
+        Note(
             index: categories.length,
             title: title,
             date: date,
-            content: content,
-          ),
-        );
+            content: content));
     notifyListeners();
   }
 
--- lib/components/screen/notes/display_note_screen.dart
+++ lib/components/screen/notes/display_note_screen.dart
@@ -18,9 +18,12 @@ class DisplayNoteScreen extends StatelessWidget {
     final categoryNote = notes[indexCategory];
     final note = categoryNote.notes[indexNote];
     final actualDate = context.watch<TimesProvider>().actualTime;
+    bool theme = context.watch<NotesProvider>().toggleThemeDisplayNote;
 
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: theme
+          ? context.watch<NotesProvider>().getColorCategory()
+          : Colors.white,
       body: Theme(
         data: Theme.of(context).copyWith(
           textSelectionTheme: TextSelectionThemeData(
@@ -30,7 +33,7 @@ class DisplayNoteScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const _MyCustomAppBar(),
+            _MyCustomAppBar(),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(30, 11, 30, 21),
@@ -59,7 +62,7 @@ class DisplayNoteScreen extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      note.date == '' ? note.date : actualDate,
+                      note.date != '' ? note.date : actualDate,
                       style: TextStyle(
                         fontSize: 16,
                         color: Colors.black.withOpacity(.5),
@@ -100,45 +103,72 @@ class DisplayNoteScreen extends StatelessWidget {
 }
 
 class _MyCustomAppBar extends StatelessWidget {
-  const _MyCustomAppBar();
-
   @override
   Widget build(BuildContext context) {
+    final provider = context.read<NotesProvider>();
+    bool theme = provider.toggleThemeDisplayNote;
+
     return SafeArea(
       child: SizedBox(
         height: 74,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(23, 16, 23, 0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 23, 0),
-                child: SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: Card(
-                    elevation: 0,
-                    color: const Color(0xFFD9D9D9).withOpacity(0.5),
-                    child: InkWell(
-                      onTap: () {
-                        context.read<NotesProvider>().updateInBack();
-                        context.pop();
-                      },
-                      borderRadius: BorderRadius.circular(10),
-                      child: Icon(
-                        Icons.arrow_back_ios_new,
-                        size: 24,
-                        color: Colors.black.withOpacity(0.5),
+        child: Container(
+          color: theme ? provider.getColorCategory() : Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(23, 16, 23, 0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 23, 0),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Card(
+                      elevation: 0,
+                      color: const Color(0xFFD9D9D9).withOpacity(0.5),
+                      child: InkWell(
+                        onTap: () {
+                          if (provider.isChanged) {
+                            provider.updateDate();
+                          }
+
+                          provider.updateInBack();
+                          context.pop();
+                        },
+                        borderRadius: BorderRadius.circular(10),
+                        child: Icon(
+                          Icons.arrow_back_ios_new,
+                          size: 24,
+                          color: Colors.black.withOpacity(0.5),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const Spacer(),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: SizedBox(
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Card(
+                      elevation: 0,
+                      color: const Color(0xFFD9D9D9).withOpacity(0.5),
+                      child: InkWell(
+                        onTap: () {
+                          provider.toggleThemeNote();
+                        },
+                        borderRadius: BorderRadius.circular(10),
+                        child: Icon(
+                          Icons.light_mode_outlined,
+                          size: 24,
+                          color: Colors.black.withOpacity(.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
                   width: 50,
                   height: 50,
                   child: Card(
@@ -146,38 +176,21 @@ class _MyCustomAppBar extends StatelessWidget {
                     color: const Color(0xFFD9D9D9).withOpacity(0.5),
                     child: InkWell(
                       onTap: () {
-                        // TODO: Change color
+                        provider.removeNote(
+                            provider.indexCategory, provider.indexNote);
+                        context.pop();
                       },
                       borderRadius: BorderRadius.circular(10),
                       child: Icon(
-                        Icons.light_mode_outlined,
+                        Icons.delete_outline,
                         size: 24,
                         color: Colors.black.withOpacity(.5),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 50,
-                height: 50,
-                child: Card(
-                  elevation: 0,
-                  color: const Color(0xFFD9D9D9).withOpacity(0.5),
-                  child: InkWell(
-                    onTap: () {
-                      // TODO: delete
-                    },
-                    borderRadius: BorderRadius.circular(10),
-                    child: Icon(
-                      Icons.delete_outline,
-                      size: 24,
-                      color: Colors.black.withOpacity(.5),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
